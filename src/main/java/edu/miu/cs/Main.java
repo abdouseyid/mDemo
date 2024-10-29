2e02088dea6de8c8554d9a1ef49fce3902a2febb
abdou <abdosseyed@gmail.com>
2024-10-29 12:00:32 -0500

    testing CI with Maven

--- src/main/java/edu/miu/cs/Main.java
+++ src/main/java/edu/miu/cs/Main.java
@@ -7,5 +7,9 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
+
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Goodbye!");
     }
 }
